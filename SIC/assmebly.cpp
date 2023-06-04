571724bbca4ee375bba44eb3fad1483f998b59c9
asddzxcc1856 <t7878780@gmail.com>
2023-06-04 20:28:51 +0800

    first commit

---
+++ SIC/assmebly.cpp
@@ -0,0 +1,8 @@
+#include "pass1_2.h"
+
+int main ()
+{
+	pass1();
+	pass2();
+	return 0;
+}
---
+++ SIC_XE/assmebly.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "pass1_2.h"
+
+int main ()
+{
+	pass1();
+	pass2();
+	return 0;
+}
